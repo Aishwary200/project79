fb327f1eb75984013e07e71ae483fe28a0ba05a3
Aishwary200 <amritakeshri3059@gmail.com>
2021-03-13 21:38:12 +0530

    added exchange and home screen

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SignUpLoginScreen from './screens/SignUpLoginScreen'
+import HomeScreen from './screens/HomeScreen'
+import ExchangeScreen from './screens/ExchangeScreen'
+import { createAppContainer, createSwitchNavigator, } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <SignUpLoginScreen />
+        <AppContainer />
       </View>
     );
   }
@@ -21,3 +25,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+const TabNavigator = createBottomTabNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Exchange: {
+    screen: ExchangeScreen,
+  }
+})
+const switchNavigator = createSwitchNavigator({
+  WelcomeScreen: { screen: SignUpLoginScreen },
+  TabNavigator: { screen: TabNavigator }
+})
+
+const AppContainer = createAppContainer(switchNavigator);
